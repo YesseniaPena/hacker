0c998f25b2e16811bfcbe618ffb4ad29e3436d7c
Yesseniapena <yesseniapena219@gmail.com>
2020-01-27 20:20:30 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      newsData: [], 
+      searchResults: [],
+      searchText: "",
+    }
+  }
+
+componentDidMount() {
+  this.fetchingStories();
+
+}
+
+fetchingStories () {
+  fetch('http://hn.algolia.com/api/v1/search?query=foo&tags=story')
+  .then((res) => { return res.json() })
+  .then((res) => { console.log(res); this.setState( {newsData: res.hits})})
+  .catch(error => console.log("Parsing failed", error));
+}
+search = () => {
+  var topsearch = this.state.newsData.filter(item => item.author == this.state.searchText)
+  this.setState({searchResults: topsearch})
+}
+  render () {
+    console.log(this.state.searchText)
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input type = "text" onChange ={(e) => {
+          this.setState({searchText: e.target.value})
+        }}></input>
+        <button onClick = {this.search}>search</button>
+        {this.state.searchResults.map((item,index) => {
+          console.log(item)
+          return <div key={index}>{item.author}: {item.title}</div>
+        })}
+      </div>
+
     </div>
-  );
+    );
+  }
 }
 
 export default App;
